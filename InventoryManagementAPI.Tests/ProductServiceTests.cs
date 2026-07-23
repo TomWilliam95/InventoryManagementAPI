7b84ad391b28d30b39ae79361f8d9b56b771942e
Thomas Williams <thomas.williams@hotmail.com.au>
2026-07-24 09:53:18 +1000

    Add CI/CD ProductServiceTest GetALlProducts

--- InventoryManagementAPI.Tests/ProductServiceTests.cs
+++ InventoryManagementAPI.Tests/ProductServiceTests.cs
@@ -22,30 +22,7 @@ namespace InventoryManagementAPI.Tests
             var (productRepository, supplierRepository, categoryRepository) = CreateMockRepo();
 
             productRepository.Setup(repo => repo.GetProductAsync(It.IsAny<int>()))
-                .ReturnsAsync(new Product
-                {
-                    ID = It.IsAny<int>(),
-                    Sku = "TestSku",
-                    Name = "Test Product",
-                    Description = "Test Description",
-                    CategoryID = It.IsAny<int>(),
-                    SupplierID = It.IsAny<int>(),
-                    Category = new Category
-                    {
-                        ID = It.IsAny<int>(),
-                        Name = It.IsAny<string>(),
-                        Description = It.IsAny<string>()
-                    },
-                    Supplier = new Supplier
-                    {
-                        ID = It.IsAny<int>(),
-                        Name = It.IsAny<string>(),
-                        ContactName = It.IsAny<string>(),
-                        PhoneContact = It.IsAny<string>(),
-                        EmailContact = It.IsAny<string>(),
-                        Address = It.IsAny<string>()
-                    }
-                });
+                .ReturnsAsync(CreateProduct(It.IsAny<string>()));
 
             // Act
             var service = new ProductService(productRepository.Object, supplierRepository.Object, categoryRepository.Object);
@@ -176,6 +153,79 @@ namespace InventoryManagementAPI.Tests
         }
 
 
+        // === GET ALL PRODUCTS TESTS === \\
+        [Fact]
+        public async Task GetProducts_GetProductsSuccess_Return200()
+        {
+            // Arrange
+            var (productRepository, supplierRepository, categoryRepository) = CreateMockRepo();
+            productRepository.Setup(repo =>
+            repo.GetAllProductsAsync()).
+            ReturnsAsync(new List<Product>
+            {
+                CreateProduct(It.IsAny<string>()),
+                CreateProduct(It.IsAny<string>()),
+                CreateProduct(It.IsAny<string>())
+            });
+
+            //Act
+            var service = new ProductService(productRepository.Object, supplierRepository.Object, categoryRepository.Object);
+            var result = await service.GetAllProducts();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.True(result.Success);
+            Assert.Equal(200, result.StatusCode);
+            Assert.Equal("Successfully Retrieved All Products", result.Message);
+
+            //Verify
+            productRepository.Verify(repo => repo.GetAllProductsAsync(), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetProducts_GetProductsWhenNoProductsExist_Returns404()
+        {
+            //Arrange
+            var (productRepository, supplierRepository, categoryRepository) = CreateMockRepo();
+            productRepository.Setup(repo => repo.GetAllProductsAsync())
+                .ReturnsAsync([]);
+
+            //Act
+            var service = new ProductService(productRepository.Object, supplierRepository.Object, categoryRepository.Object);
+            var result = await service.GetAllProducts();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.False(result.Success);
+            Assert.Equal(404, result.StatusCode);
+            Assert.Equal("No Products Found", result.Message);
+
+            //Verify
+            productRepository.Verify(repo => repo.GetAllProductsAsync(), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetProducts_GetProductsThrowsException_Return500()
+        {
+            //Arrange
+            var (productRepository, supplierRepository, categoryRepository) = CreateMockRepo();
+            productRepository.Setup(repo => repo.GetAllProductsAsync())
+                .ThrowsAsync(new Exception("Database connection error"));
+
+            //Act
+            var service = new ProductService(productRepository.Object, supplierRepository.Object, categoryRepository.Object);
+            var result = await service.GetAllProducts();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.False(result.Success);
+            Assert.Equal(500, result.StatusCode);
+            Assert.Equal("Internal error occurred, failed to load all products.", result.Message);
+
+            //Verify
+            productRepository.Verify(repo => repo.GetAllProductsAsync(), Times.Once);
+        }
+
 
         // === CREATE MOQ REPO HELPER METHOD === \\
         private (Mock<IProductRepository>, Mock<ISupplierRepository>, Mock<ICategoryRepository>) CreateMockRepo()
@@ -184,6 +234,35 @@ namespace InventoryManagementAPI.Tests
             var supplierRepoMock = new Mock<ISupplierRepository>();
             var categoryRepoMock = new Mock<ICategoryRepository>();
             return (productRepoMock, supplierRepoMock, categoryRepoMock);
+        }
+
+        // === CREATE PRODUCT HELPER METHOD === \\
+        private Product CreateProduct(string testName)
+        {
+            return new Product
+            {
+                ID = It.IsAny<int>(),
+                Sku = testName,
+                Name = testName,
+                Description = It.IsAny<string>(),
+                CategoryID = It.IsAny<int>(),
+                SupplierID = It.IsAny<int>(),
+                Category = new Category
+                {
+                    ID = It.IsAny<int>(),
+                    Name = It.IsAny<string>(),
+                    Description = It.IsAny<string>()
+                },
+                Supplier = new Supplier
+                {
+                    ID = It.IsAny<int>(),
+                    Name = It.IsAny<string>(),
+                    ContactName = It.IsAny<string>(),
+                    PhoneContact = It.IsAny<string>(),
+                    EmailContact = It.IsAny<string>(),
+                    Address = It.IsAny<string>()
+                }
+            };
         }
     }
 }
